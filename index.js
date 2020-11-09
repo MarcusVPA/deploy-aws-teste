863117a720d2a3122cd0a1b4504f458e4e17bb05
Marcus Vinicius <marcus_mayme@hotmail.com>
2020-11-09 14:17:57 -0300

    NodeJs + AWS teste

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = 3333;
+
+const app = express();
+
+app.get('/',(req,resp)=>{
+    return resp.status(200).json({message: 'Server is up Baby ;D'})
+});
+
+app.listen(port,(err)=>{
+    err ? console.log("Erro ao executar o server.") : console.log("Executando o server na porta " +port); 
+});
