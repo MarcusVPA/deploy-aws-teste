d63e01ed1bf609c806db9441637f576528be5407
Marcus Vinicius <marcus_mayme@hotmail.com>
2020-11-25 16:13:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/',(req,resp)=>{
     return resp.status(200).json({message: 'Server is up Baby ;D'})
 });
 
+app.get('/atualizou',(req,resp)=>{
+    return resp.status(200).json({message: 'Atualizou mesmo!!!'})
+});
+
 app.listen(port,(err)=>{
     err ? console.log("Erro ao executar o server.") : console.log("Executando o server na porta " +port); 
 });
